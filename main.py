83b3858562485b788b686bb030ec1286b9e92be2
PabRubio <pablo.rubio.punal@gmail.com>
2025-08-02 21:09:07 +0100

    Create shit pipeline

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from transformers import pipeline
-import torch
+from langchain.prompts import PromptTemplate
+from langchain_huggingface import HuggingFacePipeline
 
-# Check if GPU is available
-print(torch.cuda.is_available())
-print(torch.cuda.get_device_name(0))
+image = "selfie.jpg"
 
-model = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
-response = model("selfie.jpg")
-print(response)
+caption_pipeline = pipeline("image-to-text", model="Salesforce/blip-image-captioning-large")
+
+text_pipeline = pipeline("text-generation", model="openai-community/gpt2")
+generator = HuggingFacePipeline(pipeline=text_pipeline)
+
+template = PromptTemplate.from_template("Elaborate on this image caption: {caption}")
+
+chain = template | generator
+
+caption = caption_pipeline(image)
+
+elaborated = chain.invoke({"caption": caption[0]['generated_text']})
+
+print(elaborated)
