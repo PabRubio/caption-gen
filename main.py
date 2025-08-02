4e578bd847418dc9736a41f31b1eb88eb372fd37
PabRubio <pablo.rubio.punal@gmail.com>
2025-08-02 22:58:59 +0100

    Revert gpt2 pipeline

--- main.py
+++ main.py
@@ -9,17 +9,6 @@ image = "selfie.jpg"
 
 caption_pipeline = pipeline("image-to-text", model="Salesforce/blip-image-captioning-large")
 
-text_pipeline = pipeline("text-generation", model="openai-community/gpt2")
-generator = HuggingFacePipeline(pipeline=text_pipeline)
-
-template = PromptTemplate.from_template("Elaborate on this image caption: {caption}")
-
-chain = template | generator
-
 caption = caption_pipeline(image)
-
-elaborated = chain.invoke({"caption": caption[0]['generated_text']})
-
-print(elaborated)
 
 print(caption)
