eba87c940e899bf473a3d04ee024a57345896769
PabRubio <pablo.rubio.punal@gmail.com>
2025-08-02 13:41:23 +0100

    Check GPU

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from transformers import pipeline
+import torch
+
+# Check if GPU is available
+print(torch.cuda.is_available())
+print(torch.cuda.get_device_name(0))
 
 model = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
 response = model("selfie.jpg")
