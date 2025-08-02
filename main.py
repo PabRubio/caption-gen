2b19f460381d96ed9b05fc1d0cd1ca8f63d8d313
PabRubio <pablo.rubio.punal@gmail.com>
2025-08-02 23:07:34 +0100

    Add caption kwargs

--- main.py
+++ main.py
@@ -9,6 +9,17 @@ image = "selfie.jpg"
 
 caption_pipeline = pipeline("image-to-text", model="Salesforce/blip-image-captioning-large")
 
-caption = caption_pipeline(image)
+caption = caption_pipeline(
+    image,
+    generate_kwargs={
+        "max_new_tokens": 50,
+        "num_beams": 4,
+        "do_sample": False,
+        "repetition_penalty": 1.2,
+        "length_penalty": 1.0,
+        "early_stopping": True,
+        "no_repeat_ngram_size": 3
+    }
+)
 
 print(caption)
